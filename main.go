fc5f2ebf988033cd761abd91f716843f64f788ed
Sheshagiri <shmallipedhi@extremenetworks.com>
2018-11-02 15:48:43 +0530

    added greet, health and version endpoints

--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
+
 import (
-  "net/http"
-  "strings"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
 )
+
+const version = "1.0.0"
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Hello " + message
-  w.Write([]byte(message))
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message + "\nversion: " + version
+	fmt.Fprintf(w, message)
 }
+
+func checkHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
+
+func printVersion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, version)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	h := t.Hour()
+	path := r.URL.Path
+	user := strings.SplitAfter(path, "/greet/")[1]
+	const (
+		morning   = "Good Morning, "
+		afternoon = "Good Afternoon, "
+		evening   = "Good Evening, "
+	)
+
+	var message string
+
+	switch {
+	case h >= 0 && h < 12:
+		message = morning
+	case h >= 12 && h < 18:
+		message = afternoon
+	case h >= 18:
+		message = evening
+	}
+	message = message + user + "!\n\n" + t.Format(time.ANSIC)
+	fmt.Fprintf(w, message)
+}
+
 func main() {
-  http.HandleFunc("/", sayHello)
-  if err := http.ListenAndServe(":8000", nil); err != nil {
-    panic(err)
-  }
+	http.HandleFunc("/health", checkHealth)
+	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/greet/", greet)
+	http.HandleFunc("/version", printVersion)
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		panic(err)
+	}
+
 }
